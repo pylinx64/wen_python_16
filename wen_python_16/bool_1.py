df51d696435b49a2fe3a65d2907df94355762e60
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-24 20:02:46 +0200

    Add files via upload

---
+++ wen_python_16/bool_1.py
@@ -0,0 +1,21 @@
+print(10 + 10)
+print('10 + 10')
+print('10' + '10')
+print(int('10') + int('10'))
+#print(b+a)
+#x = input()
+#print(int(x) + 10)
+
+print('a' + 'b')
+print('a + b')
+
+a = None
+print(a)
+
+print(10 / 2)
+print(10 * 2)
+print(1 - 5)
+
+import os
+os.system('color 70')
+print('*   *  *' * 100000000)
