d552f38db5b82a38c69a124c8fca7e5ce973d02b
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-10 20:10:58 +0200

    Add files via upload

--- wen_python_16/bot.py
+++ wen_python_16/bot.py
@@ -1,5 +1,14 @@
 import telebot
 
-bot = telebot.TeleBot('')	# сюда токен
+bot = telebot.TeleBot('1619506853:AAG29EDn_qKFkdHQSKCNw0gWnkYC_anS-HQ')	# сюда токен
 
+@bot.message_handler(commands=['start'])
+def text_start(message):
+	'''Принимает команду старт и отвечает на нее'''
+	bot.send_message(message.chat.id, 'Привет! Я ретро бот с ретро картинками')
+	print(message.chat.first_name, message.text)
+	
 
+print('# start bot ...')	
+bot.polling()
+print('# stop bot ...')
