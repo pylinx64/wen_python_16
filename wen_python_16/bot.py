1aecb61fc396d7bd8137609192da6709ba902538
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-24 20:03:26 +0200

    Add files via upload

--- wen_python_16/bot.py
+++ wen_python_16/bot.py
@@ -1,4 +1,5 @@
 import telebot
+import random
 
 bot = telebot.TeleBot('1619506853:AAG29EDn_qKFkdHQSKCNw0gWnkYC_anS-HQ')	# сюда токен
 
@@ -18,6 +19,18 @@ def text_bot(message):
 		bot.send_message(message.chat.id, 'нормально')
 	else:
 		bot.send_message(message.chat.id, 'я не распознаю такой команды')
+	
+	
+@bot.message_handler(content_types=['sticker'])
+def sticker_bot(message):
+	'''Принимает стикер и отвечает на него'''
+	#print(message.sticker.file_id)
+	stickers = ['CAACAgIAAxkBAAMNYFtYqsmr0aW9xg3RtBR4Y6fBNQgAAh8AA8A2TxPkODP3hFKZkh4E',
+	'CAACAgIAAxkBAAMNYFtYqsmr0aW9xg3RtBR4Y6fBNQgAAh8AA8A2TxPkODP3hFKZkh4E', ]
+	
+	one_sticker = random.choice(stickers)
+	
+	bot.send_sticker(message.chat.id, one_sticker)
 	
 
 print('# start bot ...')	
